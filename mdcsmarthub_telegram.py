7e535289dd7aece8a9f69258a4dd2a14537299e2
Mirko Daniele Comparetti <comparetti.mirko@gmail.com>
2021-02-06 10:25:57 +0100

    Added env variables, and implemented first mqtt connection and updated requirements

--- mdcsmarthub_telegram.py
+++ mdcsmarthub_telegram.py
@@ -5,6 +5,10 @@ Simple Bot to handle the MDC SmartHome
 import json
 import os
 import logging
+import paho.mqtt.client as mqtt
+import uuid
+import json
+from rfc3986 import urlparse
 import signal
 from telegram import Update, KeyboardButton
 from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, Filters, CallbackContext
@@ -34,6 +38,8 @@ class TelegramBotCore:
         self.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, self.generic))
 
         self.allowedchatids = [i for i in os.environ.get('MDCSMARTHUB_TELEGRAM_CHATID').split(" ")]
+
+        self.logger.info("Done initialize class TelegramBotCore")
 
     def cleanup(self, signum, frame):
         self.logger.debug("Cleanup")
@@ -110,6 +116,57 @@ class TelegramBotCore:
         self.updater.idle()
 
 
+class MqttClientCore:
+    """Main app."""
+
+    def __init__(self):
+        """Init MqttClientCore class."""
+        self.logger = logging.getLogger("mdcsmarthub_telegram" + ".MqttClientCore")
+        self.logger.info("Begin initialize class MqttClientCore")
+        topic_prefix = os.environ.get('MDCSMARTHUB_MQTT_TOPIC_PREFIX', "telegrambot")
+        self.mqtt_topic_prefix = topic_prefix if topic_prefix.endswith("/") else (topic_prefix + "/")
+        self.mqtt_broker_url = None
+        self.mqtt_broker_port = None
+        self.mqtt_broker_user = None
+        if not self.validate_broker_info(os.environ.get('MDCSMARTHUB_MQTT_BROKER', "mqtt://test.mosquitto.org:1883")):
+            self.logger.error("Broker information not valid")
+            return
+
+        self.mqtt_client = mqtt.Client(client_id=str(uuid.uuid4()))
+        self.mqtt_client.on_connect = self.on_connect
+        self.mqtt_client.on_publish = self.on_publish
+
+        self.logger.info("Done initialize class MqttClientCore")
+
+    def validate_broker_info(self, broker_info):
+        self.logger.debug("Validating " + broker_info)
+        parseduri = urlparse(broker_info)
+        if not (parseduri.scheme in ["mqtt", "ws"]):
+            return False
+        self.mqtt_broker_url = parseduri.host
+        self.mqtt_broker_port = parseduri.port
+        self.mqtt_broker_user = parseduri.userinfo
+        self.logger.debug("broker_user {}".format(self.mqtt_broker_user))
+        self.logger.debug("broker_url {}, broker_port: {}".format(self.mqtt_broker_url, self.mqtt_broker_port))
+        if not (self.mqtt_broker_url and self.mqtt_broker_port):
+            return False
+        return True
+
+    def on_connect(self, client, userdata, flags, rc):
+        self.logger.info("Connected with result code " + str(rc))
+
+    def on_publish(self, client, userdata, result):
+        pass
+
+    def connect(self):
+        if self.mqtt_broker_url and self.mqtt_broker_port:
+            self.logger.debug("{}:{}".format(self.mqtt_broker_url, self.mqtt_broker_port))
+            self.mqtt_client.connect(self.mqtt_broker_url, self.mqtt_broker_port, 30)
+
+    def start(self):
+        self.mqtt_client.loop_start()
+        
+
 # Enable logging
 logging.basicConfig(
     # filename='/var/log/mdc/mdcsmarthub_telegram.log',
@@ -122,7 +179,9 @@ logger.setLevel(os.environ.get('MDCSMARTHUB_TELEGRAM_LOGLEVEL', logging.INFO))
 if __name__ == "__main__":
     # Start MDCSmartHub Telegram app
     logger.info("Starting MDCSmartHub Telegram service")
-    root = TelegramBotCore()
-    logger.info("Run main loop - wait for stop signal")
-    root.loop()
+    mqtt_instance = MqttClientCore()
+    mqtt_instance.connect()
+    bot_instance = TelegramBotCore()
+    logger.info("Run main bot loop - wait for stop signal")
+    bot_instance.loop()
     logger.info("Stopping main loop")
